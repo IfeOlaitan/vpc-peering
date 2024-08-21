2cc4ff6c82f629eb31de10081050a1ad7fa3c39c
IfeOlaitan <ifeoluwa1201@gmail.com>
2024-08-21 05:20:03 +0100

    fixed config errors

--- infra/nat-gateway.tf
+++ infra/nat-gateway.tf
@@ -5,7 +5,7 @@ resource "aws_eip" "prod_eip" {
 
 # prod nat gateway
 resource "aws_nat_gateway" "prod_nat" {
-  allocation_id = aws_eip.prod_eip
+  allocation_id = aws_eip.prod_eip.id
   subnet_id     = aws_subnet.prod_web.id
   connectivity_type = "public"
 
--- infra/subnet-association.tf
+++ infra/subnet-association.tf
@@ -1,37 +1,37 @@
 # prod subnet + route association
 resource "aws_route_table_association" "web" {
-  subnet_id      = aws_subnet.prod_web
+  subnet_id      = aws_subnet.prod_web.id
   route_table_id = aws_route_table.prod_public.id
 }
 
 resource "aws_route_table_association" "app1" {
-  subnet_id      = aws_subnet.prod_app1
+  subnet_id      = aws_subnet.prod_app1.id
   route_table_id = aws_route_table.prod_private_with_int.id
 }
 
 resource "aws_route_table_association" "dbcache" {
-  subnet_id      = aws_subnet.prod_dbcache
+  subnet_id      = aws_subnet.prod_dbcache.id
   route_table_id = aws_route_table.prod_private_with_int.id
 }
 
 resource "aws_route_table_association" "app2" {
-  subnet_id      = aws_subnet.prod_app2
+  subnet_id      = aws_subnet.prod_app2.id
   route_table_id = aws_route_table.prod_private.id
 }
 
 resource "aws_route_table_association" "db" {
-  subnet_id      = aws_subnet.prod_db
+  subnet_id      = aws_subnet.prod_db.id
   route_table_id = aws_route_table.prod_private.id
 }
 
 
 # prod subnet + route association
-resource "aws_route_table_association" "web" {
-  subnet_id      = aws_subnet.dev_web
+resource "aws_route_table_association" "web2" {
+  subnet_id      = aws_subnet.dev_web.id
   route_table_id = aws_route_table.dev_public.id
 }
 
-resource "aws_route_table_association" "db" {
-  subnet_id      = aws_subnet.dev_db
+resource "aws_route_table_association" "db2" {
+  subnet_id      = aws_subnet.dev_db.id
   route_table_id = aws_route_table.dev_private.id
 }
